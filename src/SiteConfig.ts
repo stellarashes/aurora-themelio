217b660a89eeda537816edac02548a3a4bfc0fe8
Eugene Chang <xmc3ify@gmail.com>
2016-09-05 18:25:02 -0400

    Set sync to default to true in dev

--- src/SiteConfig.ts
+++ src/SiteConfig.ts
@@ -14,12 +14,13 @@ export class SiteConfig {
     public static CacheHost: string = process.env.CACHE_HOST || 'localhost';
     public static SitePort: number = process.env.PORT || 3000;
     public static ScanPaths: string = process.env.SCAN_PATHS || './controllers,./services,./models';
+
     public static DatabaseUser: string = process.env.DB_USER || 'demo';
     public static DatabasePass: string = process.env.DB_PASS || '';
     public static DatabaseHost: string = process.env.DB_HOST || 'localhost';
     public static DatabaseSchema: string = process.env.DB_SCHEMA || 'demo';
     public static DatabasePort: number = process.env.DB_PORT || 3306;
     public static DatabaseDialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql' = 'mysql';
-    public static DatabaseSync: boolean = typeof(process.env.DB_SYNC) !== 'undefined' ? process.env.DB_SYNC : true;
+    public static DatabaseSync: boolean = typeof(process.env.DB_SYNC) !== 'undefined' ? process.env.DB_SYNC : process.env.NODE_ENV === 'development';
     public static DatabaseLog: Function = process.env.NODE_ENV === 'development' ? console.log : null;
 }
